cb1fae88c9f06c405b1fd82d73fbb4bb987a48c1
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-09-07 18:04:58 +0100

    Initial commit, setup folder structure and general pseudocode

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+//PSEUDOCODE
+
+// Create a java library project
+// Create 3 different packages revolving around Book, Library and User
+
+//BOOK
+// Add properties of number, title, author, genre, subGenre, publisher, isAvailable
+// Maybe add genre of each book as an enum of tech, science, nonfiction, fiction, philosophy
+//
+
+//LIBRARY
+// Add properties of Books
+// Add method registerUser
+//
+//
+//
+//
+
+//USER
+// Add properties of name, borrowedBooks, isAdmin, dateOfBirth
+// Add method borrowBook
+// Add method returnBook
+//
+//
+//
+//
+//
+//
+//
+//
